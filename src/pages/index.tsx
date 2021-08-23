04b5016c9298a07675801323e7ca1dcf00fee99c
unknown <thien.nguyen.cs2704@gmail.com>
2021-08-24 00:00:05 +0700

    add head-title for home-page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -12,6 +12,7 @@ import {
   DiscoverThing,
   NearbyLocation,
 } from "../types/types";
+import HeadLayout from "../components/Layout/HeadLayout";
 
 export interface CardsProps {
   nearbyLocations?: NearbyLocation[];
@@ -54,19 +55,21 @@ const Home: React.FC<CardsProps> = ({
   discoverThings,
 }) => {
   return (
-    <div className="flex min-h-screen mx-auto">
-      <Script
-        src="https://maps.googleapis.com/maps/api/js?key=AIzaSyB3p_GqRnG15VxQoZ7auxNg8dxbJOEEu94&libraries=places"
-        strategy="beforeInteractive"
-      />
-      <main className="w-full px-6 mx-auto max-w-screen-2xl md:px-14">
-        <ExploreNearby nearbyLocations={nearbyLocations} />
-        <FlexibleBanner />
-        <LiveAnywhere anywhereCategories={anywhereCategories} />
-        <HostingBanner />
-        <DiscoverThings discoverThings={discoverThings} />
-      </main>
-    </div>
+    <HeadLayout title="Airbnb | Home">
+      <div className="flex min-h-screen mx-auto">
+        <Script
+          src="https://maps.googleapis.com/maps/api/js?key=AIzaSyB3p_GqRnG15VxQoZ7auxNg8dxbJOEEu94&libraries=places"
+          strategy="beforeInteractive"
+        />
+        <main className="w-full px-6 mx-auto max-w-screen-2xl md:px-14">
+          <ExploreNearby nearbyLocations={nearbyLocations} />
+          <FlexibleBanner />
+          <LiveAnywhere anywhereCategories={anywhereCategories} />
+          <HostingBanner />
+          <DiscoverThings discoverThings={discoverThings} />
+        </main>
+      </div>
+    </HeadLayout>
   );
 };
 
